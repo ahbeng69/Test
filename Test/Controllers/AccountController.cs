37352c44b99012fc7619a4b194f283f08033c794
jason_wong <jason_wong@executivecentre.com>
2013-07-10 11:45:42 +0800

    Douche

--- Test/Controllers/AccountController.cs
+++ Test/Controllers/AccountController.cs
@@ -41,7 +41,7 @@ namespace Test.Controllers
             }
 
             // If we got this far, something failed, redisplay form
-            ModelState.AddModelError("", "The user name or password provided is incorrect.");
+            ModelState.AddModelError("", "The user name or password provided is incorrect douche.");
             return View(model);
         }
 
